128d719b76c12840f78ecb2fd51e0fdb0b40bf60
estrella <epopo2@illinois.edu>
2022-09-11 22:10:34 -0500

    Add header

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 var page; 
 var font;
+
 var githubButton;
+var linkedinButton;
+var resumeButton;
+
 var fontFile = "assets/Chivo-Regular.ttf"; // font for text
 var buttons = new Array(3); 
 let x;
@@ -33,16 +37,23 @@ function homePage() {
   textSize(height/16);
   textAlign(CENTER, TOP);
   text("Hey, it's Ellie!", 0, windowHeight/8, windowWidth, windowHeight-windowHeight/16); //writes the text 
-  fill(100, 55, 44);
+  fill(63, 108, 81);
 
   
 
   // buttons 
   githubButton = createImg('assets/github-logo.gif');
-  githubButton.position(windowWidth/2.8, windowHeight/5);
+  githubButton.position(0, windowHeight/9, windowWidth, windowHeight-windowHeight/16);
+  // githubButton.center();
+  if (windowWidth < 500) {
+    githubButton.position(10,10);
+  }
   githubButton.addClass('github-button');
 
-
+  linkedinButton = createImg('assets/linkedin-pixel.PNG');
+  // linkedinButton.position(0, 0, windowWidth, windowHeight-windowHeight/16);
+  linkedinButton.center("horizontal");
+  linkedinButton.addClass('linkedin-button');
 
 }
 
