2d1bf096b1b75bff90c2cd2004a179290421b5f4
estrella <epopo2@illinois.edu>
2022-12-19 22:17:10 -0600

    Add back button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+      colors: {
+          transparent: 'transparent',
+          gray: colors.trueGray,
+          white: colors.white,
+          indigo: colors.indigo,
+          black: colors.black,
+      },
+      extend: {
+          colors: {
+            transparent: 'transparent',
+            gray: colors.trueGray,
+            white: colors.white,
+            indigo: colors.indigo,
+            black: colors.black,
+          },
+      },  
   },
   plugins: [],
 }
